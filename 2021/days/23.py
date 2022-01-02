b4b4ada0bdd46f50ada628c881bea5cdeacd2e01
StijnSlot <stynder@live.nl>
2022-01-02 12:57:15 +0100

    Fixed day 23 and solved day 25

--- 2021/days/23.py
+++ 2021/days/23.py
@@ -1,5 +1,4 @@
 from functools import cache
-from util import aoc
 
 
 def map_char_to_score(c: str):
@@ -13,9 +12,7 @@ def map_char_to_room(c: str, room_size: int):
 
 
 def hallway_clear(i, j, hallway, room_size):
-    if j == 0 and hallway[1] != ".":
-        return False
-    if j == len(hallway) - 1 and hallway[-2] != ".":
+    if j <= 0 and hallway[1] != ".":
         return False
     if j <= 1 and i >= room_size and hallway[2] != ".":
         return False
@@ -23,24 +20,26 @@ def hallway_clear(i, j, hallway, room_size):
         return False
     if j <= 3 and i >= 3 * room_size and hallway[4] != ".":
         return False
-    if j >= len(hallway) - 2 and i < 3 * room_size and hallway[4] != ".":
+    if j >= len(hallway) - 1 and hallway[-2] != ".":
         return False
-    if j >= len(hallway) - 3 and i < 2 * room_size and hallway[3] != ".":
+    if j >= len(hallway) - 2 and i < 3 * room_size and hallway[-3] != ".":
         return False
-    if j >= len(hallway) - 4 and i < room_size and hallway[2] != ".":
+    if j >= len(hallway) - 3 and i < 2 * room_size and hallway[-4] != ".":
+        return False
+    if j >= len(hallway) - 4 and i < room_size and hallway[-5] != ".":
         return False
     return True
 
 
 def penalty(i, j, c, room_size):
-    room = (i // room_size) * room_size + room_size
+    room = (i // room_size) * 2 + 2
     hallway = j if j < 2 else j + 4 if j > 4 else 2 * j - 1
     dis = abs(room - hallway) + i % room_size + 1
     return map_char_to_score(c) * dis
 
 
 @cache
-def dfs(rooms, hallway, room_size, kkk):
+def dfs(rooms, hallway, room_size):
     # print(rooms, hallway)
     sol = 99999999999999999999
     dis = 0
@@ -53,21 +52,21 @@ def dfs(rooms, hallway, room_size, kkk):
             j = map_char_to_room(x, room_size)
             if not hallway_clear(j, i, hallway, room_size):
                 continue
-            for k in reversed(range(j, j+room_size)):
-                if all(rooms[l] == "." for l in range(j, k+1)):
-                    dis += penalty(k, i, x, room_size)
-                    rooms = rooms[:k] + x + rooms[k+1:]
-                    hallway = hallway[:i] + "." + hallway[i+1:]
-                    changes = True
-                    break
-                elif rooms[k] != x:
-                    break
+            if all(room != '.' for room in rooms[j:j+room_size]):
+                continue
+            if any(room not in [x, '.'] for room in rooms[j:j+room_size]):
+                continue
+            k = next(k for k in reversed(range(j, j+room_size)) if rooms[k] == '.')
+            dis += penalty(k, i, x, room_size)
+            rooms = rooms[:k] + x + rooms[k+1:]
+            hallway = hallway[:i] + "." + hallway[i+1:]
+            changes = True
     if all(r != '.' and 0 <= i - map_char_to_room(r, room_size) < room_size for i, r in enumerate(rooms)):
         return dis
     for i, x in enumerate(rooms):
         if x == ".":
             continue
-        if i % room_size > 0 and any(rooms[i-k-1] != "." for k in range(i % room_size)):
+        if any(rooms[i-k-1] != "." for k in range(i % room_size)):
             continue
         dest_room = map_char_to_room(x, room_size)
         if 0 <= i - dest_room < room_size and all(rooms[k] == x for k in range(i, dest_room + room_size)):
@@ -77,9 +76,7 @@ def dfs(rooms, hallway, room_size, kkk):
                 continue
             if not hallway_clear(i, j, hallway, room_size):
                 continue
-            kkk += dis + penalty(i, j, x, room_size)
-            sol = min(sol, dfs(rooms[:i] + "." + rooms[i+1:], hallway[:j] + x + hallway[j + 1:], room_size, kkk)
-                      + dis + penalty(i, j, x, room_size))
+            sol = min(sol, dfs(rooms[:i] + "." + rooms[i+1:], hallway[:j] + x + hallway[j + 1:], room_size) + dis + penalty(i, j, x, room_size))
     return sol
 
 
@@ -98,10 +95,11 @@ def part_two(data: str) -> int:
     room1 = grid[2][5] + "CB" + grid[3][5]
     room2 = grid[2][7] + "BA" + grid[3][7]
     room3 = grid[2][9] + "AC" + grid[3][9]
-    return dfs(room0 + room1 + room2 + room3, ".......", 4, 0)
+    return dfs(room0 + room1 + room2 + room3, ".......", 4)
 
 
 if __name__ == "__main__":
-    input_data = aoc.get_input(2021, 23)
+    with open('../data/23.txt') as f:
+        input_data = f.read()
     # print(part_one(input_data))
     print(part_two(input_data))
--- 2021/days/25.py
+++ 2021/days/25.py
@@ -1,8 +1,34 @@
-from util import aoc
+from copy import deepcopy
 
 
 def part_one(data: str) -> int:
-    return None
+    grid = [[c for c in row] for row in data.splitlines()]
+    moved = True
+    steps = 0
+    while moved:
+        moved = False
+        new_grid = deepcopy(grid)
+        for i, row in enumerate(grid):
+            for j, cell in enumerate(row):
+                if cell == '.' and grid[i][(j - 1) % len(row)] == '>':
+                    new_grid[i][j] = '>'
+                    new_grid[i][(j - 1) % len(row)] = '.'
+                    moved = True
+        grid = new_grid
+        new_grid = deepcopy(grid)
+        for i, row in enumerate(grid):
+            for j, cell in enumerate(row):
+                if cell == '.' and grid[(i - 1) % len(grid)][j] == 'v':
+                    new_grid[i][j] = 'v'
+                    new_grid[(i - 1) % len(grid)][j] = '.'
+                    moved = True
+        steps += 1
+        grid = new_grid
+        print(steps)
+        for row in grid:
+            print("".join(row))
+    return steps
+
 
 
 def part_two(data: str) -> int:
@@ -10,6 +36,7 @@ def part_two(data: str) -> int:
 
 
 if __name__ == "__main__":
-    input_data = aoc.get_input(2021, 25)
+    with open('../data/25.txt') as f:
+        input_data = f.read()
     print(part_one(input_data))
     print(part_two(input_data))
