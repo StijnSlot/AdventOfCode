42e5d5b3a90251bd6f8c2a45d7a1e16f62aaea16
StijnSlot <stynder@live.nl>
2022-12-06 06:06:50 +0100

    Solved days 5 and 6

--- 2022/days/05.py
+++ 2022/days/05.py
@@ -1,12 +1,45 @@
 from util import aoc
+import re
 
 
-def part_one(data: str) -> int:
-    return None
+def read_data(data: str) -> tuple[list[list[str]], list[tuple[int, int, int]]]:
+    parts = data.split("\n\n")
+    lines = parts[0].splitlines()
+    nr_of_stacks = int(lines[-1].split()[-1])
+    stacks = [[] for _ in range(nr_of_stacks)]
+    for i in range(len(lines) - 1):
+        for j in range(nr_of_stacks):
+            if len(lines[i]) > j * 4 + 1 and lines[i][j * 4 + 1] != ' ':
+                stacks[j].append(lines[i][j * 4 + 1])
+    commands = []
+    for line in parts[1].splitlines():
+        match = re.match("move (\d+) from (\d+) to (\d+)", line)
+        x, y, z = match.groups()
+        commands.append((int(x), int(y) - 1, int(z) - 1))
+    return stacks, commands
 
 
-def part_two(data: str) -> int:
-    return None
+def perform_moves(stacks: list[list[str]], commands: list[tuple[int, int, int]], reverse: bool = False) -> list[list[str]]:
+    for x, y, z in commands:
+        stacks[z] = (stacks[y][:x][::-1] if reverse else stacks[y][:x]) + stacks[z]
+        stacks[y] = stacks[y][x:]
+    return stacks
+
+
+def result(stacks: list[list[str]]) -> str:
+    return "".join(x[0] if len(x) > 0 else ' ' for x in stacks)
+
+
+def part_one(data: str) -> str:
+    stacks, commands = read_data(data)
+    perform_moves(stacks, commands, reverse=True)
+    return result(stacks)
+
+
+def part_two(data: str) -> str:
+    stacks, commands = read_data(data)
+    perform_moves(stacks, commands)
+    return result(stacks)
 
 
 if __name__ == "__main__":
--- 2022/days/06.py
+++ 2022/days/06.py
@@ -1,12 +1,19 @@
 from util import aoc
 
 
+def start_of_marker(data: str, x: int) -> int:
+    for i in range(len(data) - x):
+        if len(set(data[i:i+x])) == x:
+            return i + x
+    return -1
+
+
 def part_one(data: str) -> int:
-    return None
+    return start_of_marker(data, 4)
 
 
 def part_two(data: str) -> int:
-    return None
+    return start_of_marker(data, 14)
 
 
 if __name__ == "__main__":
