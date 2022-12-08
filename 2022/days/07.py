03bad4905f5a495499d8ecae9674d2d0c28a1196
StijnSlot <stynder@live.nl>
2022-12-08 07:12:11 +0100

    Solved days 7 and 8

--- 2022/days/07.py
+++ 2022/days/07.py
@@ -1,12 +1,73 @@
 from util import aoc
+from itertools import count
+from dataclasses import dataclass, field
+
+
+_part_one_limit = 100000
+_total_disk_space = 70000000
+_needed_space = 30000000
+
+
+@dataclass()
+class Node:
+    id: int = field(default_factory=count().__next__, init=False)
+    name: str
+    parent: int
+    file_size: int = 0
+    children: list[int] = field(default_factory=list, init=False)
+    _total_size: int = -1
+
+    def cd(self, x: str, dirs: dict) -> int:
+        if x == '..':
+            if self.parent == -1:
+                raise ValueError("cd called from root")
+            return self.parent
+        for child_id in self.children:
+            if dirs[child_id].name == x:
+                return child_id
+        raise ValueError("cd directory not found in current directory")
+
+    def total_size(self, dirs: dict) -> int:
+        if self._total_size != -1:
+            return self._total_size
+        self._total_size = self.file_size + sum(dirs[x].total_size(dirs) for x in self.children)
+        return self._total_size
+
+
+def parse_data(data: str) -> tuple[int, dict[int, Node]]:
+    cur_dir = Node('/', -1)
+    root_id = cur_dir.id
+    dirs = {root_id: cur_dir}
+    for line in data.splitlines():
+        if line.startswith("$ cd"):
+            _, _, cd = line.split()
+            if cd == '/':
+                cur_dir = dirs[root_id]
+                continue
+            next_dir_id = cur_dir.cd(cd, dirs)
+            cur_dir = dirs[next_dir_id]
+        elif line.startswith("dir"):
+            _, x = line.split()
+            if x not in {dirs[y].name for y in cur_dir.children}:   # check for duplicate ls calls, not really needed
+                new_dir = Node(x, cur_dir.id)
+                dirs[new_dir.id] = new_dir
+                cur_dir.children.append(new_dir.id)
+        elif not line.startswith("$ ls"):
+            # must be a line with file size, e.g. 123 a.bat
+            x, _ = line.split()
+            cur_dir.file_size += int(x)
+    return root_id, dirs
 
 
 def part_one(data: str) -> int:
-    return None
+    _, dirs = parse_data(data)
+    return sum(x.total_size(dirs) for x in dirs.values() if x.total_size(dirs) < _part_one_limit)
 
 
 def part_two(data: str) -> int:
-    return None
+    root_id, dirs = parse_data(data)
+    extra_space_needed = _needed_space + dirs[root_id].total_size(dirs) - _total_disk_space
+    return min(x.total_size(dirs) for x in dirs.values() if extra_space_needed <= x.total_size(dirs))
 
 
 if __name__ == "__main__":
--- 2022/days/08.py
+++ 2022/days/08.py
@@ -1,12 +1,46 @@
 from util import aoc
 
 
+def visible(i, j, grid):
+    if i == 0 or i + 1 == len(grid) or j == 0 or j + 1 == len(grid[i]):
+        return True
+    return grid[i][j] > max(grid[k][j] for k in range(0, i))\
+           or grid[i][j] > max(grid[k][j] for k in range(i+1, len(grid))) \
+           or grid[i][j] > max(grid[i][:j]) \
+           or grid[i][j] > max(grid[i][j+1:])
+
+
+def scene_score_dir(i, j, grid, di, dj):
+    x, y = i + di, j + dj
+    result = 0
+    while 0 <= x < len(grid) and 0 <= y < len(grid[i]):
+        result += 1
+        if grid[x][y] >= grid[i][j]:
+            return result
+        x += di
+        y += dj
+    return result
+
+
+def scene_score(i, j, grid):
+    return scene_score_dir(i, j, grid, 0, 1) \
+           * scene_score_dir(i, j, grid, 1, 0) \
+           * scene_score_dir(i, j, grid, 0, -1) \
+           * scene_score_dir(i, j, grid, -1, 0)
+
+
+def read_grid(data: str) -> list[list[int]]:
+    return [[int(x) for x in line] for line in data.splitlines()]
+
+
 def part_one(data: str) -> int:
-    return None
+    grid = read_grid(data)
+    return sum(1 for i, line in enumerate(grid) for j, _ in enumerate(line) if visible(i, j, grid))
 
 
 def part_two(data: str) -> int:
-    return None
+    grid = read_grid(data)
+    return max(scene_score(i, j, grid) for i, line in enumerate(grid) for j, _ in enumerate(line))
 
 
 if __name__ == "__main__":
