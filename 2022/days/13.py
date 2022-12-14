a31ee197dd01e5816a405bd275c9886aae86b3b0
StijnSlot <stynder@live.nl>
2022-12-14 06:42:24 +0100

    Solved days 13 and 14

--- 2022/days/13.py
+++ 2022/days/13.py
@@ -1,12 +1,65 @@
 from util import aoc
+from functools import cmp_to_key
+
+
+_start_divider = [['2']]
+_end_divider = [['6']]
+
+
+def parse(line, i=1):
+    items = []
+    item = ""
+    while i <= len(line):
+        x = line[i]
+        if x == '[':
+            item, i = parse(line, i+1)
+            items.append(item)
+            i += 1
+            continue
+        elif x == ']':
+            if item != "":
+                items.append(item)
+            return items, i
+        if x == ',':
+            items.append(item)
+            item = ""
+        else:
+            item += x
+        i += 1
+    raise ValueError("Line did not correctly parse")
+
+
+def right_order(items1, items2):
+    if items1 == items2:
+        return 0
+    if type(items1) != list and type(items2) != list:
+        return 1 if int(items1) <= int(items2) else -1
+    if type(items1) != list:
+        return right_order([items1], items2)
+    if type(items2) != list:
+        return right_order(items1, [items2])
+    for x, y in zip(items1, items2):
+        a = right_order(x, y)
+        if a > 0:
+            return 1
+        if a < 0:
+            return -1
+    return 1 if len(items1) < len(items2) else -1 if len(items1) > len(items2) else 0
 
 
 def part_one(data: str) -> int:
-    return None
+    pairs = [tuple(parse(line)[0] for line in pairs.splitlines()) for pairs in data.split('\n\n')]
+    return sum(i + 1 for i, (x, y) in enumerate(pairs) if right_order(x, y) > 0)
 
 
 def part_two(data: str) -> int:
-    return None
+    lines = [_start_divider] + \
+            [parse(line)[0] for pairs in data.split('\n\n') for line in pairs.splitlines()] + \
+            [_end_divider]
+    lines.sort(key=cmp_to_key(right_order), reverse=True)
+    i = lines.index(_start_divider) + 1
+    j = lines.index(_end_divider) + 1
+    return i * j
 
 
 if __name__ == "__main__":
--- 2022/days/14.py
+++ 2022/days/14.py
@@ -1,12 +1,64 @@
 from util import aoc
 
 
+_start_x, _start_y = 500, 0
+
+
+def simulate_sand(grid, startx, starty):
+    x, y = startx, starty
+    rest = 0
+    while y + 1 < len(grid):
+        if grid[y+1][x] == '.':
+            y += 1
+        elif grid[y+1][x-1] == '.':
+            y += 1
+            x -= 1
+        elif grid[y+1][x+1] == '.':
+            y += 1
+            x += 1
+        else:
+            # rest
+            grid[y][x] = '+'
+            rest += 1
+            x, y = startx, starty
+            if grid[y][x] != '.':
+                return rest
+    return rest
+
+
+def parse_lines(data: str) -> list[list[tuple[int, ...]]]:
+    return [[tuple(int(x) for x in coord.split(',')) for coord in line.split(' -> ')] for line in data.splitlines()]
+
+
+def create_grid(lines: list[list[tuple[int, ...]]], add_floor: bool) -> list[list[str]]:
+    max_y = max(x[1] for line in lines for x in line)
+    max_x = max(x[0] for line in lines for x in line)
+    grid = [['.' for _ in range(2 * max_x)] for _ in range(max_y + 2)]
+    if add_floor:
+        grid.append(['#' for _ in range(2 * max_x)])
+    for line in lines:
+        x, y = line[0]
+        for new_x, new_y in line[1:]:
+            grid[y][x] = '#'
+            dx = (new_x - x) // abs(new_x - x) if new_x != x else 0
+            dy = (new_y - y) // abs(new_y - y) if new_y != y else 0
+            while x != new_x or y != new_y:
+                x += dx
+                y += dy
+                grid[y][x] = '#'
+    return grid
+
+
 def part_one(data: str) -> int:
-    return None
+    lines = parse_lines(data)
+    grid = create_grid(lines, False)
+    return simulate_sand(grid, _start_x, _start_y)
 
 
 def part_two(data: str) -> int:
-    return None
+    lines = parse_lines(data)
+    grid = create_grid(lines, True)
+    return simulate_sand(grid, _start_x, _start_y)
 
 
 if __name__ == "__main__":
