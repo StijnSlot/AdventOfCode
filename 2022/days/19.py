14917e818affb23bce59822a9984f04c494efbf9
StijnSlot <stynder@live.nl>
2022-12-20 08:05:19 +0100

    Solved days 19 and 20

--- 2022/days/19.py
+++ 2022/days/19.py
@@ -1,12 +1,104 @@
 from util import aoc
+import re
+from dataclasses import dataclass
+from operator import mul
+from functools import reduce
+
+
+@dataclass
+class Robot:
+    makes: str
+    costs: list[tuple[str, int]]
+    max_nr: int = 99999
+
+
+def update_items(items, creates, robot=None):
+    new_items = {**items}
+    for c in creates:
+        new_items[c] += creates[c]
+    if robot is not None:
+        for c, k in robot.costs:
+            new_items[c] -= k
+    return new_items
+
+
+def update_creates(creates, robot):
+    new_creates = {**creates}
+    new_creates[robot.makes] += 1
+    return new_creates
+
+
+def remove_items(items, creates):
+    new_items = {**items}
+    for c in creates:
+        new_items[c] -= creates[c]
+    return new_items
+
+
+def quality(blueprint, minutes, items, creates):
+    if minutes <= 0:
+        return items['geode']
+    result = 0
+    old_items = remove_items(items, creates)
+
+    # buy other robots
+    for robot in blueprint[::-1]:
+        # dont create robot if already at max nr or could already buy before
+        if creates[robot.makes] < robot.max_nr and \
+                all(items[c] >= k for c, k in robot.costs) and \
+                not all(old_items[c] >= k for c, k in robot.costs):
+            result = max(result, quality(blueprint,
+                                         minutes - 1,
+                                         update_items(items, creates, robot),
+                                         update_creates(creates, robot)))
+
+    return max(result, quality(blueprint,
+                               minutes - 1,
+                               update_items(items, creates),
+                               creates))
+
+
+def read_data(data: str):
+    blueprints = []
+    re_str = "(Blueprint (\w+): )*Each (\w+) robot costs (\d+) (\w+)( and (\d+) (\w+))*"
+    for line in data.splitlines():
+        robot_strs = line.split('. ')
+        robots = []
+        for x in robot_strs:
+            match = re.match(re_str, x)
+            a = match.groups()
+            creates = a[2]
+            costs = [(a[4], int(a[3]))]
+            if a[5] is not None:
+                costs.append((a[7], int(a[6])))
+            robots.append(Robot(creates, costs))
+        for robot in robots:
+            if robot.makes == 'geode':
+                continue
+            robot.max_nr = max(k for robot2 in robots for c, k in robot2.costs if c == robot.makes)
+        blueprints.append(robots)
+
+    return blueprints
+
+
+def simulate(blueprints, minutes):
+    results = []
+    for i, blueprint in enumerate(blueprints):
+        creates = {robot.makes: 0 for robot in blueprint}
+        items = {**creates}
+        creates['ore'] = 1
+        results.append(quality(blueprint, minutes, items, creates))
+    return results
 
 
 def part_one(data: str) -> int:
-    return None
+    blueprints = read_data(data)
+    return sum((i + 1) * x for i, x in enumerate(simulate(blueprints, 24)))
 
 
 def part_two(data: str) -> int:
-    return None
+    blueprints = read_data(data)
+    return reduce(mul, simulate(blueprints, 32))
 
 
 if __name__ == "__main__":
--- 2022/days/20.py
+++ 2022/days/20.py
@@ -1,12 +1,33 @@
 from util import aoc
 
 
+def mixin(nums: list[tuple[int, int]]) -> list[tuple[int, int]]:
+    N = len(nums)
+    for i in range(N):
+        val, j = nums[i]
+        new_j = (j + val - 1) % (N - 1) + 1
+        nums = [(n, k - (j < k <= new_j) + (new_j <= k < j)) for n, k in nums]
+        nums[i] = (val, new_j)
+    return nums
+
+
+def solution(nums: list[tuple[int, int]]) -> int:
+    k = next(x[1] for x in nums if x[0] == 0)
+    return sum(next(x[0] for x in nums if x[1] == (k + i * 1000) % len(nums))
+               for i in range(1, 4))
+
+
 def part_one(data: str) -> int:
-    return None
+    nums = [(int(x), i) for i, x in enumerate(data.splitlines())]
+    nums = mixin(nums)
+    return solution(nums)
 
 
 def part_two(data: str) -> int:
-    return None
+    nums = [(int(x) * 811589153, i) for i, x in enumerate(data.splitlines())]
+    for _ in range(10):
+        nums = mixin(nums)
+    return solution(nums)
 
 
 if __name__ == "__main__":
